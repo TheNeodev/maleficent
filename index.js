9d42576dcd62a7d0a4381d8037046ab689cd46e9
NeoDev <neoforevershog@gmail.com>
2025-09-16 18:50:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
 console.log('🕒 Starting Maleficent . . .');
-const { join } = require('path');
-const { fork } = require('child_process');
-const start = () => {
-    const p = fork(join(__dirname, 'main.js'), process.argv.slice(2), {
-            stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-        })
-        .on('message', data => {
-            if (data === 'reset') {
-                console.log('🕒 Restarting Maleficent . . .');
-                p.kill()
-            }
-            if (data === 'uptime') {
-                p.send(process.uptime());
-            }
-        })
-        .on('exit', code => {
-            console.log('Exited with code:', code);
-            start()
-        });        
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+process.on('uncaughtException', (e) => { 
+    const error = String(e);
+    if (error.includes('rate-overlimit')) {
+        return false;   
+    } else if (error.includes('internal-server-error')) {
+        return false;
+    } else {
+        console.error(e);
+    }
+});
+
+const { Format, Connect, Signal } = require('utils-mf'); 
+
+Object.assign(global, { 
+    default_db: { 
+        users: {}, 
+        chats: {}, 
+        settings: {}, 
+        stores: {}, 
+        menfess: {}, 
+        contacts: {} 
+    }, 
+    setting: require('./config.json'), 
+    mess: require('./lib/message.js'), 
+    Connect: Connect, 
+    Format: Format, 
+    moment: require("moment-timezone") 
+});
+
+moment.tz.setDefault("Asia/Jakarta").locale("id");
+
+require('./lib/settings.js');
+require('utils-mf/index.js');
+require('./lib/system.js');
+require('./lib/src/cloud/mongo-db.js');
+require('./lib/src/cloud/github-db.js');
+
+const startWhatsApp = async () => {
+    const conn = await Signal({ sessions: './sessions' }); 
+    require('./lib/system.js').caller(conn);
+    conn.ev.on('connection.update', (update) => {
+        const { connection } = update;
+        if (connection === 'open') {
+            console.log(`🟢 Online`);
+        } else if (connection === 'connecting') {
+            console.log(`🟡 Reconnecting`);
+        } else if (connection === 'close') {
+            console.log(`🔴 Disconnected`);
+            return startWhatsApp();
+        }
+    });
 };
-start()
+
+startWhatsApp();
